7c7a365b251ea91138de9a9f815573ce3ee0915f
Anurag Narula <anurag519narula@gmail.com>
2025-01-12 01:08:54 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+import { FaFilePdf } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center text-3xl">
+        <FaFilePdf
+          onClick={() => {
+            window.open(
+              "https://drive.google.com/file/d/1UaQ5EW9ve_2TmPHtkfTNukBmdE_UrUsS/view?usp=sharing"
+            ),
+              "_blank";
+          }}
+          style={{ cursor: "pointer" }}
+        />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-3xl">
+        <FaLinkedin
+          onClick={() => {
+            window.open("https://www.linkedin.com/in/anurag-narula-83035b213/"),
+              "_blank";
+          }}
+          style={{ cursor: "pointer" }}
+        />
+        <FaGithub
+          onClick={() => {
+            window.open("https://github.com/Anurag519Narula"), "_blank";
+          }}
+          style={{ cursor: "pointer" }}
+        />
+        <SiLeetcode
+          onClick={() => {
+            window.open("https://leetcode.com/u/anurag519narula/"), "_blank";
+          }}
+          style={{ cursor: "pointer" }}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,59 @@
+import BookVerse from "../assets/projects/BookVerse.jpg";
+import Portfolio from "../assets/projects/Portfolio.jpg";
+import SpendSense from "../assets/projects/SpendSense.jpg";
+import CarRentalSystem from "../assets/projects/CarRentalSystem.webp";
+
+export const HERO_CONTENT = `I am a dedicated Full Stack Web Developer with hands-on experience in building responsive and feature-rich web applications. Proficient in modern front-end frameworks like React.js and robust backend technologies, I specialize in developing user-centric applications that offer seamless performance.My expertise includes crafting scalable solutions, integrating APIs, and ensuring intuitive user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Jan 2024 - June 2024",
+    role: "Software Development Intern",
+    company: "MakeMyTrip",
+    description: `Contributed to the development of web applications using cutting edge technology. Expanded Hotels LOB on MMT's US Website. Implemented various features such as Error Boundary, Multi Factor Authentication for users. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["ReactJS", "React-Redux", "Saga Middleware", "Github"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "SpendSense",
+    image: SpendSense,
+    link: "https://github.com/Anurag519Narula/SpendSense",
+    description:
+      "Developed an expense tracker app using the MERN stack for seamless transaction management with real-time updates.",
+    technologies: ["React JS", "Node.js", "MongoDB", "Express JS"],
+  },
+  {
+    title: "BookVerse",
+    image: BookVerse,
+    link: "https://github.com/Anurag519Narula/BookVerse",
+    description:
+      "Constructed a project from scratch leveraging the Google Books API to fetch and display book data, enabling users to directly search for book titles and explore 40+ results.",
+    technologies: ["React JS", "Node.js", "MongoDB", "Express JS"],
+  },
+  {
+    title: "Portfolio Website",
+    image: Portfolio,
+    link: "https://github.com/Anurag519Narula/Portfolio-Website",
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["React JS", "Tailwind CSS"],
+  },
+  {
+    title: "Car Rental System",
+    image: CarRentalSystem,
+    link: "https://github.com/Anurag519Narula/Car-Rental-System",
+    description:
+      " Developed an OOPs based car rental project enhancing efficiency, user experience to streamline the rental process.",
+    technologies: ["CPP"],
+  },
+];
+
+export const CONTACT = {
+  address: "37 Rose Enclave ,Civil Lines, Ludhiana, Punjab",
+  phoneNo: "+91 98770-40456 ",
+  email: "anurag519narula@gmail.com",
+};
